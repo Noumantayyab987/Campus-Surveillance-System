980312dc6292e74cb45e43ff048cca1f79eb1f82
unknown <70078229@student.uol.edu.pk>
2023-05-15 17:08:16 +0500

    Fix URL3

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DashboardLayout from './components/DashboardLayout';
-import Dashboard from './pages/Dashboard.jsx';
-import About from './pages/UploadVideoFootage.jsx';
-import AddLiveCameras from './pages/AddLiveCameras.jsx';
-import Comment from './pages/DownloadLogFIle.jsx';
-import Product from './pages/Product.jsx';
-import ProductList from './pages/ProductList.jsx';
-import Registration from "./Registration.js";
+import Dashboard from './pages/Dashboard';
+import UploadVideoFootage from './pages/UploadVideoFootage';
+import AddLiveCameras from './pages/AddLiveCameras';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+import Registration from './Registration';
 import { AuthProvider } from './AuthContext';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Registration><Registration /></Registration>} />
+          <Route path="/" element={<Registration />} />
           <Route path="/dashboard" element={<DashboardLayout><Dashboard /></DashboardLayout>} />
-          <Route path="/UploadVideoFootage" element={<DashboardLayout><About /></DashboardLayout>} />
-          <Route path="/DownloadLogFIle" element={<DashboardLayout><Comment /></DashboardLayout>} />
+          <Route path="/UploadVideoFootage" element={<DashboardLayout><UploadVideoFootage /></DashboardLayout>} />
           <Route path="/AddLiveCameras" element={<DashboardLayout><AddLiveCameras /></DashboardLayout>} />
           <Route path="/product" element={<DashboardLayout><Product /></DashboardLayout>} />
           <Route path="/productList" element={<DashboardLayout><ProductList /></DashboardLayout>} />
         </Routes>
       </AuthProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
 
