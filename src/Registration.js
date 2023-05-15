7578aadb62792db0900aabb6ffa392f35dbb3aa2
unknown <70078229@student.uol.edu.pk>
2023-05-15 19:27:18 +0500

    fix package file

--- src/Registration.js
+++ src/Registration.js
@@ -48,7 +48,7 @@ function Registration() {
         // Set cookies
         document.cookie = `access_token=${data.access_token}`;
         document.cookie = `token_type=${data.token_type}`;
-        window.location.href = "https://astonishing-lokum-6bc615.netlify.app/dashboard"; // redirect to dashboard
+        window.location.href = "https://gregarious-pothos-f687f0.netlify.app/dashboard"; // redirect to dashboard
       }
     } catch (error) {
       console.error(error);
